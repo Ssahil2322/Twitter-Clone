729cb3c2c8810a1f2f744b7a53c0f09a7d5ad90f
Sahil Bhagat <s.sahil2322@gmail.com>
2023-09-06 01:23:38 -0400

    environment setup

---
+++ app/_app.tsx
@@ -0,0 +1,12 @@
+import { AppProps } from 'next/app'
+import './globals.css'
+import Layout from '@/components/layout'
+
+export default function App({Component,pageProps}: AppProps){
+  return (
+    <Layout>
+      <Component {...pageProps}/>
+      </Layout>
+
+    )
+}
---
+++ components/layout.tsx
@@ -0,0 +1,10 @@
+interface LayoutProps{
+    children:React.ReactNode;
+}
+
+const Layout:React.FC<LayoutProps>=({children})=>{
+    return(
+        <div>{children}</div>
+    )
+}
+export default Layout;
